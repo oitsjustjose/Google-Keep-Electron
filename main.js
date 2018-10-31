5a0b70905e4a2aac8b55770ccd2638ec43d4f566
oitsjustjose <stovallj1995@gmail.com>
2018-10-30 22:55:52 -0500

    Update things!

--- main.js
+++ main.js
@@ -6,14 +6,19 @@ const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
+const shell = require('electron').shell
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    titleBarStyle: 'hidden'
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -33,13 +38,45 @@ function createWindow () {
     mainWindow = null
   })
 
+
+
   mainWindow.setMenu(null);
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function () {
+  createWindow();
+  mainWindow.webContents.on('will-navigate', function (e, reqUrl) {
+    console.log("hi")
+    let getHost = url => require('url').parse(url).host;
+    let reqHost = getHost(reqUrl);
+    let isExternal = reqHost && reqHost != getHost(wc.getURL());
+    if (isExternal) {
+      e.preventDefault();
+      electron.shell.openExternal(reqUrl);
+    }
+  });
+})
+
+// Listen for web contents being created
+app.on('web-contents-created', (e, contents) => {
+
+  // Check for a webview
+  if (contents.getType() == 'webview') {
+
+    // Listen for any new window events
+    contents.on('new-window', (e, url) => {
+      e.preventDefault()
+      shell.openExternal(url)
+    })
+    contents.on('will-navigate', (e, url) => {
+      e.preventDefault()
+      shell.openExternal(url)
+    })
+  }
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -57,6 +94,8 @@ app.on('activate', function () {
     createWindow()
   }
 })
+
+
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
