d033b468593ee13d743c156ad2d5357e6447a69c
oitsjustjose <stovallj1995@gmail.com>
2018-10-30 23:17:49 -0500

    Cleanups

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const electron = require('electron')
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
-
+const Menu = electron.Menu
 const path = require('path')
 const url = require('url')
 const shell = require('electron').shell
@@ -12,90 +12,139 @@ const shell = require('electron').shell
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+function createMenu() {
+  var template = [
+    {
+      label: 'Google Keep',
+      submenu: [
+        { label: 'About Google Keep', role: 'about' },
+        { type: 'separator' },
+        { role: 'services', submenu: [] },
+        { type: 'separator' },
+        { role: 'hide' },
+        { role: 'hideothers' },
+        { role: 'unhide' },
+        { type: 'separator' },
+        { role: 'quit' }
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'Command+Z',
+          selector: 'undo:'
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+Command+Z',
+          selector: 'redo:'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Cut',
+          accelerator: 'Command+X',
+          selector: 'cut:'
+        },
+        {
+          label: 'Copy',
+          accelerator: 'Command+C',
+          selector: 'copy:'
+        },
+        {
+          label: 'Paste',
+          accelerator: 'Command+V',
+          selector: 'paste:'
+        },
+        {
+          label: 'Select All',
+          accelerator: 'Command+A',
+          selector: 'selectAll:'
+        }
+      ]
+    },
+    {
+      label: 'Window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'Command+M',
+          selector: 'performMiniaturize:'
+        },
+        {
+          label: 'Close',
+          accelerator: 'Command+W',
+          selector: 'performClose:'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Bring All to Front',
+          selector: 'arrangeInFront:'
+        },
+      ]
+    },
+  ];
+
+  var menu = Menu.buildFromTemplate(template);
+
+  Menu.setApplicationMenu(menu); // Must be called within app.on('ready', function(){ ... });
+}
+
 function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    titleBarStyle: 'hidden'
+    titleBarStyle: 'hidden',
+    title: 'Google Keep',
+    icon: __dirname + '/icon.png'
   })
 
-  // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   })
-
-
 
   mainWindow.setMenu(null);
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', function () {
   createWindow();
-  mainWindow.webContents.on('will-navigate', function (e, reqUrl) {
-    console.log("hi")
-    let getHost = url => require('url').parse(url).host;
-    let reqHost = getHost(reqUrl);
-    let isExternal = reqHost && reqHost != getHost(wc.getURL());
-    if (isExternal) {
-      e.preventDefault();
-      electron.shell.openExternal(reqUrl);
-    }
-  });
+  createMenu();
 })
 
-// Listen for web contents being created
 app.on('web-contents-created', (e, contents) => {
-
-  // Check for a webview
   if (contents.getType() == 'webview') {
-
-    // Listen for any new window events
     contents.on('new-window', (e, url) => {
       e.preventDefault()
-      shell.openExternal(url)
+      mainWindow.loadURL(url)
+      // shell.openExternal(url)
     })
     contents.on('will-navigate', (e, url) => {
       e.preventDefault()
-      shell.openExternal(url)
+      mainWindow.loadURL(url)
     })
   }
 })
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow()
   }
 })
-
-
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
