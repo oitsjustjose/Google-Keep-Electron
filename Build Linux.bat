0e7f876161cc37621bb781b6ca528bf902c08743
oitsjustjose <stovallj1995@gmail.com>
2018-10-31 08:50:28 -0500

    Remove batch files

--- Build Linux.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-electron-packager . "Google Keep" --overwrite --asar=true --platform=linux --arch=x64 --icon=img/icon.png --prune=true --out=release-builds
--- Build Windows.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-electron-packager . "Google Keep" --overwrite --asar=true --platform=win32 --arch=ia32 --icon=icon.ico --prune=true --out=release-builds --version-string.CompanyName=YourName --version-string.FileDescription="A description" --version-string.ProductName="A name"
